642017e1a5521b161a05c3df59ef459cb0a6335e
Vinicius Eustaquio Cortez <viniciusecortez@outlook.com>
2020-05-24 02:26:36 -0300

    Criado novo model DailyKnown
    Inaugurado o primeiro teste

--- written/models.py
+++ written/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.utils.translation import ugettext_lazy as _
 # Create your models here.
+class DailyKnown(models.Model):
+
+    date = models.DateField(auto_now=True)
+    title = models.CharField(max_length=300)
+
+    class Meta:
+        verbose_name = _("DailyKnown")
+        verbose_name_plural = _("DailyKnowns")
+
+    def __str__(self):
+        return f"{self.title} - {self.date.strftime('%Y-%m-%d')}"
--- written/tests.py
+++ written/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from written.models import DailyKnown
+from datetime import datetime
 
 # Create your tests here.
+class DailyKnowsModelTest(TestCase):
+    def setUp(self):
+        main_class = DailyKnown()
+        main_class.date = datetime.strptime('2020-03-02', '%Y-%m-%d')
+        main_class.title = 'Teste test'
+        self.main_class = main_class
+
+    def  test_str(self):
+        now = str(self.main_class)
+        expected = 'Teste test - 2020-03-02'
+        self.assertEquals(now, expected)
